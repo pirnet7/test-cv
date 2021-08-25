5d7644aca7dbe83d60b245f85c5a91a0cd86127e
Berkelmann <daniel.berkelmann@swisscom.com>
2021-08-25 13:54:56 +0200

    Adding SDX typescript types

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@swisscom/sdx/dist/css/webcomponents.css'
-import { defineCustomElements } from "@swisscom/sdx/dist/js/webcomponents/loader";
+import { defineCustomElements, JSX } from "@swisscom/sdx/dist/js/webcomponents/loader";
+
+// Register the Stencil types
+type StencilProps<T> = {
+    [P in keyof T]?: Omit<T[P], "ref"> | HTMLAttributes<T>;
+};
+
+// Register the React types
+type ReactProps<T> = {
+    [P in keyof T]?: DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>;
+};
+
+// Combine both types
+type StencilToReact<T = JSX.IntrinsicElements, U = HTMLElementTagNameMap> = StencilProps<T> &
+    ReactProps<U>;
+
+// Export the new types as the new JSX namespace.
+// Disable the eslint errors
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    export namespace JSX {
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface IntrinsicElements extends StencilToReact {}
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
