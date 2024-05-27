6fcd30b924854de1bd6681e6143f8b949bbed5d9
Varrese Bruno, GHR-POD-NEX-31 <Bruno.Varrese@swisscom.com>
2024-05-27 13:42:16 +0200

    test commit

--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,17 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
-import i18next from "i18next";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  debug: true,
-  fallbackLang: "en",
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json", // Anpassen des Pfads entsprechend deiner Ordnerstruktur im öffentlichen Ordner
+    },
+  });
 
-export default i18next;
+export default i18n;
